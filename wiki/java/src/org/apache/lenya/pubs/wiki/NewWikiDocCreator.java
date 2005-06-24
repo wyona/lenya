6622f5e42f4d8f19fcec90b71d5d202f5b35c196
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-06-24 14:03:51 +0000

    yarep added

--- wiki/java/src/org/apache/lenya/pubs/wiki/NewWikiDocCreator.java
+++ wiki/java/src/org/apache/lenya/pubs/wiki/NewWikiDocCreator.java
@@ -18,9 +18,18 @@
 package org.apache.lenya.pubs.wiki;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
 
 import org.apache.lenya.cms.authoring.DefaultCreator;
+import org.apache.lenya.xml.DocumentHelper;
 import org.apache.log4j.Category;
+
+import org.w3c.dom.Document;
+
+import org.wyona.yarep.core.Path;
+import org.wyona.yarep.core.Repository;
+import org.wyona.yarep.core.RepositoryFactory;
 
 public class NewWikiDocCreator extends DefaultCreator {
     Category log = Category.getInstance(NewWikiDocCreator.class);
@@ -63,5 +72,91 @@ public class NewWikiDocCreator extends DefaultCreator {
         log.info("Filename: " + file.getAbsolutePath());
 
         return file.getAbsolutePath();
+    }
+
+    /**
+      * DOCUMENT ME!
+      *
+      * @param samplesDir DOCUMENT ME!
+      * @param parentDir DOCUMENT ME!
+      * @param childId DOCUMENT ME!
+      * @param childType DOCUMENT ME!
+      * @param childName the name of the child
+      * @param language for which the document is created
+      * @param parameters additional parameters that can be considered when 
+      *  creating the child
+      *
+      * @throws Exception DOCUMENT ME!
+      */
+    public void create(
+        File samplesDir,
+        File parentDir,
+        String childId,
+        short childType,
+        String childName,
+        String language,
+        Map parameters)
+        throws Exception {
+        // Set filenames
+        String id = generateTreeId(childId, childType);
+        String filenameMeta = getChildMetaFileName(parentDir, id, language);
+
+        String doctypeSample = samplesDir + File.separator + getSampleResourceName();
+        //String doctypeMeta = samplesDir + File.separator + sampleMetaName;
+
+        File sampleFile = new File(doctypeSample);
+        if (!sampleFile.exists()) {
+            log.error("No such sample file: " + sampleFile + " Have you configured the sample within doctypes.xconf?");
+            throw new FileNotFoundException("" + sampleFile);
+        }
+
+        // Read sample file
+        log.debug("Read sample file: " + doctypeSample);
+
+        Document doc = DocumentHelper.readDocument(new File(doctypeSample));
+
+        log.debug("sample document: " + doc);
+
+        // transform the xml if needed
+        log.debug("transform sample file: ");
+        transformXML(doc, id, childType, childName, parameters);
+
+        // write the document (create the path, i.e. the parent
+        // directory first if needed)
+	log.error(parentDir.toString());
+	log.error(id);
+	log.error(language);
+        String filename = getChildFileName(parentDir, id, language);
+        log.error(filename);
+        File parent = new File(new File(filename).getParent());
+
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
+
+        // Write file
+        log.debug("write file: " + filename);
+        DocumentHelper.writeDocument(doc, new File(filename));
+
+	Repository repo = new RepositoryFactory().newRepository("wiki");
+        DocumentHelper.writeDocument(doc, repo.getWriter(new Path("/" + id + "_" + language + ".xml")));
+
+        // now do the same thing for the meta document if the
+        // sampleMetaName is specified
+/*
+        if (sampleMetaName != null) {
+            doc = DocumentHelper.readDocument(new File(doctypeMeta));
+
+            transformMetaXML(doc, id, childType, childName, parameters);
+
+            parent = new File(new File(filenameMeta).getParent());
+
+            if (!parent.exists()) {
+                parent.mkdirs();
+            }
+
+            DocumentHelper.writeDocument(doc, new File(filenameMeta));
+        }
+*/
     }
 }
