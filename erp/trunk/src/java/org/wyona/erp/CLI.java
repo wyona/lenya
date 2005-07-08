e0408dde79b3d2d5e2162bd02e30af7f899fd605
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-07-08 22:13:34 +0000

    help added

--- erp/trunk/src/java/org/wyona/erp/CLI.java
+++ erp/trunk/src/java/org/wyona/erp/CLI.java
@@ -9,16 +9,27 @@ public class CLI {
      *
      */
     public static void main(String[] args) {
-        if (args.length <= 2) {
-            System.out.println("Usage: REPO_CONFIG REPO_HOME --add-task");
+        if (args.length == 0) {
+            System.out.println("Usage: --help");
             return;
         }
 
+        if (args[0].equals("--help")) {
+            listHelp();
+            return;
+        }
+
+        if (args.length <= 2) {
+            System.out.println("Usage: REPO_CONFIG REPO_HOME [command]");
+            return;
+        }
         String repoConfig = args[0]; // e.g. repository.xml
         String repoHomeDir = args[1]; // e.g. repotest
         String command = args[2];
 
-        if (command.equals("--add-task")) {
+        if (command.equals("--help")) {
+            listHelp();
+	} else if (command.equals("--add-task")) {
             new ERP(repoConfig, repoHomeDir).addTask("My first task", "michi");
 	} else if (command.equals("--list-tasks")) {
             String workspaceName = "default";
@@ -26,5 +37,13 @@ public class CLI {
         } else {
             System.out.println("No such command: " + command);
         }
+    }
+
+    /**
+     *
+     */
+    public static void listHelp() {
+        System.out.println("REPO_CONFIG REPO_HOME --add-task      Add task");
+        System.out.println("REPO_CONFIG REPO_HOME --list-tasks    List tasks");
     }
 }
